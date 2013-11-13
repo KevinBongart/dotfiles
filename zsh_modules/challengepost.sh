199617d6549931072c3ccd83121d504bf77aea59
Kevin Bongart <contact@kevinbongart.net>
2013-11-13 18:56:03 -0500

    Better build command for ChallengePost

--- zsh_modules/challengepost.sh
+++ zsh_modules/challengepost.sh
@@ -1,3 +1,3 @@
-alias ci='/web/platform/build/schedule_build build --no-wait'
+alias ci='(cd /web/platform && build/schedule_build build --no-wait)'
 alias cpnginx='sudo nginx -c /web/platform/config/nginx/nginx.conf'
 alias logs='tail -f /web/platform/log/*.log'
